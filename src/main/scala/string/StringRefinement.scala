5e2814e99f7f8f3c45441ce1fb08aec3cdef5a6d
robertbutacu <robertbutacu@gmail.com>
2019-01-19 21:07:40 +0000

    added youtube uri example

--- src/main/scala/string/StringRefinement.scala
+++ src/main/scala/string/StringRefinement.scala
@@ -1,15 +1,33 @@
 package string
 
 import eu.timepit.refined._
-import eu.timepit.refined.string.MatchesRegex
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.boolean.And
+import eu.timepit.refined.collection.Contains
+import eu.timepit.refined.string.{MatchesRegex, Regex, Uri, Url}
 
 object StringRefinement extends App {
   type Nino = MatchesRegex[W. `"""abc"""` .T]
 
   val nino = refineV[Nino]("abc")
 
-  type DeparturePort = MatchesRegex[W.`"""(Cal ais)|(Coquelles)|(Dublin)|(Dunkirk)"""`.T]
+  // playing around with MatchesRegex
+  type ValidPort     = MatchesRegex[W.`"""(Cal ais)|(Coquelles)|(Dublin)|(Dunkirk)"""`.T]
+  type DeparturePort = Refined[String, ValidPort]
 
-  println(refineV[DeparturePort]("Cal ais"))
+  val x: Either[String, DeparturePort] = refineV[ValidPort]("Cal ais")
 
+  println(x)
+
+  // making sense of the Uri predicate and the And which unites 2 predicates
+  // unfortunately, can't mix Contains
+  type YoutubeRegex = MatchesRegex[W.`""".*youtube.com.*"""`.T]
+
+  type ValidYoutubeLink = Url And YoutubeRegex
+
+  type YoutubeLink = Refined[String, ValidYoutubeLink]
+
+  val y = refineV[And[Url, YoutubeRegex]]("http://www.youtube.com/watch?=adsfasdfasd")
+
+  println(y)
 }
